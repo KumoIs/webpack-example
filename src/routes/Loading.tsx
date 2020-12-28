1e6e77a792f85afe5b49e3add66f599bcc567725
Kumokumo <931321342@qq.com>
2020-12-28 17:04:49 +0800

    Update and rename SuspenseComponent.tsx to Loading.tsx

---
+++ src/routes/Loading.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Loading = (): JSX.Element => <div>正在加载页面中...</div>;
+
+export default Loading;
--- src/routes/SuspenseComponent.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const SuspenseComponent = (): JSX.Element => <div>正在加载页面中...</div>;
-
-export default SuspenseComponent;
